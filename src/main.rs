6ab9298651d1657c74e021b9838726f80a8fc210
Shou Ya <github@lain.li>
2016-12-19 18:10:35 +0800

    moved consume_updates to impl Bot

--- src/main.rs
+++ src/main.rs
@@ -17,54 +17,54 @@ use extensions::*;
 use ext_stack::ExtensionStack;
 
 fn process_message(ctx: &Context, msg: &tg::Message) {
-  println!("Got msg: {:?}", msg);
-  let mut exts = ctx.exts.borrow_mut();
-  exts.process(msg, ctx);
+    println!("Got msg: {:?}", msg);
+    let mut exts = ctx.exts.borrow_mut();
+    exts.process(msg, ctx);
 }
 
 fn serve(ctx: &mut Context) {
-  let mut listener = {
-    let api = &ctx.bot.api;
-    api.listener(tg::ListeningMethod::LongPoll(None))
-  };
+    let mut listener = {
+        let api = &ctx.bot.api;
+        api.listener(tg::ListeningMethod::LongPoll(None))
+    };
 
-  listener.listen(move |u| {
-    if let Some(msg) = u.message {
-      process_message(ctx, &msg);
-    }
-    info!("saving state");
-    ctx.save_state();
-    Ok(tg::ListeningAction::Continue)
-  }).unwrap();
+    listener.listen(move |u| {
+            if let Some(msg) = u.message {
+                process_message(ctx, &msg);
+            }
+            info!("saving state");
+            ctx.save_state();
+            Ok(tg::ListeningAction::Continue)
+        })
+        .unwrap();
 }
 
 fn main() {
-  // DEBUG
-  std::env::set_var("TELEGRAM_BOT_TOKEN",
-                    "167818725:AAHoBuwE2GGU63yrApdk4q-8xYqR8ng0v7w");
+    // DEBUG
+    std::env::set_var("TELEGRAM_BOT_TOKEN",
+                      "167818725:AAHoBuwE2GGU63yrApdk4q-8xYqR8ng0v7w");
 
-  env_logger::init().unwrap();
+    env_logger::init().unwrap();
 
-  let bot = Bot::from_env();
-  info!("Running as {:?}", bot.api.get_me());
+    let bot = Bot::from_env();
+    info!("Running as {:?}", bot.api.get_me());
 
-  info!("Eating up all previous messages!");
-  info!("Eaten {} messages", eat_updates(&bot));
+    info!("Eating up all previous messages!");
+    info!("Consumed {} messages", bot.consume_updates());
 
-  let mut ctx = {
-    let mut exts = ExtensionStack::new();
+    let mut ctx = {
+        let mut exts = ExtensionStack::new();
 
-    exts.plug(afk::Afk::new());
-    exts.plug(tracker::Tracker::new());
+        exts.plug(afk::Afk::new());
+        exts.plug(tracker::Tracker::new());
 
-    Context::new(bot, exts, "state.json".into())
-  };
+        Context::new(bot, exts, "state.json".into())
+    };
 
-  info!("Loading state");
-  ctx.load_state();
+    info!("Loading state");
+    ctx.load_state();
 
-  info!("Started serving");
-  serve(&mut ctx);
-  // exts.process(ctx);
+    info!("Started serving");
+    serve(&mut ctx);
+    // exts.process(ctx);
 }
-
