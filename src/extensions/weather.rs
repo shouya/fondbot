3893c41fcb89b290b52d8251325be800289690cd
Shou Ya <shouya@users.noreply.github.com>
2018-03-05 19:58:29 +0800

    fix a bug in decimal display

--- src/extensions/weather.rs
+++ src/extensions/weather.rs
@@ -125,7 +125,7 @@ impl Display for Caiyun {
         write!(f, "*Conditions*: {}\n", skycon).ok();
         write!(f, "*Temperature*: {:.0}℃ ({:.0}-{:.0}℃)\n", temp_curr, temp_lo, temp_hi).ok();
         write!(f, "*Humidity*: {:.0}-{:.0}%\n", hmd_lo * 100.0, hmd_hi * 100.0).ok();
-        write!(f, "*AQI*: {:.0}", self.fmt_aqi()).ok();
+        write!(f, "*AQI*: {}", self.fmt_aqi()).ok();
 
         Ok(())
     }
@@ -185,7 +185,7 @@ impl Caiyun {
             return "<not available>".into();
         }
         format!(
-            "{}, {} ({}-{})\n",
+            "{:.0}, {} ({:.0}-{:.0})\n",
             aqi_curr,
             aqi_level(aqi_curr as i32),
             aqi_lo,
