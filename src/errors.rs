65809ed752f2d0e40414863e7261d3e9b43a2ebf
Shou Ya <shouya@users.noreply.github.com>
2018-06-03 02:19:28 +0800

    error types for service/request

--- src/errors.rs
+++ src/errors.rs
@@ -1,12 +1,20 @@
+use hyper;
+
 error_chain! {
     foreign_links {
         Telegram(::telegram_bot::Error);
+        Hyper(hyper::Error);
+        Json(::serde_json::Error);
     }
 
     errors {
         Unknown(desc: String) {
             description("unknown error")
             display("Unknown error occured: {}", desc)
+        }
+        RequestError(uri: hyper::Uri) {
+            description("request error")
+            display("{} responded with a non-200 code", uri)
         }
     }
 }
